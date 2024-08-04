2f95254c8e3985bd527580b2e0a9093542b169dc
wargod <vishnuprasadvbhat@gmail.com>
2024-08-04 23:27:25 +0530

    data ingestion updates

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,49 @@ from sklearn.model_selection import train_test_split as tts
 from dataclasses import dataclass
 
 
-@dataclass
+@dataclass #we can use this we are only defining variables 
+# if we have some functions, we need to write it using constructor
 class DataIngestionCongif:
-  train_data_path : str=
+  train_data_path : str= os.path.join('artifacts', "train.csv")
+  test_data_path : str= os.path.join('artifacts', "test.csv")
+  raw_data_path : str = os.path.join('artifacts', "raw_data.csv")
+
+
+class DataIngestion:
+  def __init__(self):
+    self.ingestion_config = DataIngestionCongif()
+
+
+  def initiate_data_ingestion(self):
+    logging.info('Entered data ingestion method')
+    try: 
+      df=pd.read_csv('notebook\data\stud.csv')
+      logging.info('Exporting the Dataset as dataframe')
+
+      os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+
+      df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+      logging.info('train test split initiated')
+
+      train_set,test_set = tts(df,test_size=0.2,random_state=42)
+
+      train_set.to_csv(self.ingestion_config.train_data_path, index = False,header = True)
+
+      test_set.to_csv(self.ingestion_config.test_data_path, index = False ,header =True)
+
+      logging.info(' Ingestion of data is complete')
+
+      return (
+        self.ingestion_config.train_data_path,
+        self.ingestion_config.test_data_path
+
+      )
+    except Exception as e:
+      raise CustomException(e,sys)
+    
+
+if __name__ == '__main__':
+  obj1 = DataIngestion()
+
+  obj1.initiate_data_ingestion()
+  
